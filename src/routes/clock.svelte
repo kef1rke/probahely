ec202c11673502e9518d8c8fc5f0f82acb5a08d3
kocsisbarnabas1 <kocsisbarnabas1@gmail.com>
2022-01-20 06:52:45 +0100

    clock try

---
+++ src/routes/clock.svelte
@@ -0,0 +1,12 @@
+<script>
+	import TimePicker from 'svelte-time-picker';
+
+	let myOptions = {
+		is24h: true,
+		bgColor: '#9b2c2c',
+		hasButtons: true,
+		timeClassName: ' bg-gray-500 rounded-t-lg justify-around'
+	};
+</script>
+
+<TimePicker options={myOptions} />
