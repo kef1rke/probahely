2979076f9b50a7b5aa59f0f1b52f0d41bc63f83c
kocsisbarnabas1 <kocsisbarnabas1@gmail.com>
2022-03-28 22:59:34 +0200

    admin page zeneszlist

--- src/components/admin/adminHome.svelte
+++ src/components/admin/adminHome.svelte
@@ -3,11 +3,16 @@
 	import { session } from '$app/stores';
 	import { onMount } from 'svelte';
 
-	let userNumber, zenekarNumber;
+	let zenekarNumber, userData;
+	let promise = userData;
 
-	async function getUserNumber() {
-		const { data, error } = await supabase.from('users').select('*');
-		userNumber = data.length;
+	async function getUserData() {
+		const { data, error } = await supabase
+			.from('users')
+			.select('email, zenesz_nev, profile_picture_url, Zenekarok(zenekar_nev)')
+			.order('zenesz_nev');
+		userData = data;
+		console.log(userData);
 	}
 	async function getZenekarNumber() {
 		const { data, error } = await supabase.from('Zenekarok').select('*');
@@ -15,7 +20,7 @@
 	}
 
 	onMount(() => {
-		getUserNumber();
+		getUserData();
 		getZenekarNumber();
 	});
 </script>
@@ -42,7 +47,7 @@
 						</svg>
 					</div>
 					<div class="mx-4">
-						<h4 class="text-2xl font-semibold text-gray-700">{userNumber}</h4>
+						<h4 class="text-2xl font-semibold text-gray-700">{userData?.length}</h4>
 						<div class="text-gray-500">Felhasználók</div>
 					</div>
 				</div>
@@ -116,7 +121,7 @@
 									<th
 										class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50"
 									>
-										Status</th
+										Zenekar</th
 									>
 									<th
 										class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50"
@@ -132,331 +137,83 @@
 							</thead>
 
 							<tbody class="bg-white">
-								<tr>
-									<td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-										<div class="flex items-center">
-											<div class="flex-shrink-0 w-10 h-10">
-												<img
-													class="w-10 h-10 rounded-full"
-													src="https://source.unsplash.com/user/erondu"
-													alt="admin dashboard ui"
-												/>
-											</div>
+								{#await getUserData()}
+									<h1>Loading..</h1>
+								{:then}
+									{#each userData as data}
+										<tr>
+											<td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+												<div class="flex items-center">
+													<div class="flex-shrink-0 w-10 h-10">
+														<img
+															class="w-10 h-10 rounded-full"
+															src="https://source.unsplash.com/user/erondu"
+															alt="admin dashboard ui"
+														/>
+													</div>
 
-											<div class="ml-4">
-												<div class="text-sm font-medium leading-5 text-gray-900">John Doe</div>
-											</div>
-										</div>
-									</td>
+													<div class="ml-4">
+														<div class="text-sm font-medium leading-5 text-gray-900">
+															{data?.zenesz_nev}
+														</div>
+													</div>
+												</div>
+											</td>
 
-									<td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-										<div class="text-sm leading-5 text-gray-500">john@example.com</div>
-									</td>
+											<td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+												<div class="text-sm leading-5 text-gray-500">{data?.email}</div>
+											</td>
 
-									<td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-										<span
-											class="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full"
-											>Active</span
-										>
-									</td>
+											<td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+												<span
+													class="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full"
+													>{#if data.Zenekarok?.zenekar_nev == undefined}
+														Nincs zenekar
+													{:else}
+														{data.Zenekarok?.zenekar_nev}
+													{/if}</span
+												>
+											</td>
 
-									<td
-										class="px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200"
-									>
-										<svg
-											xmlns="http://www.w3.org/2000/svg"
-											class="w-6 h-6 text-blue-400"
-											fill="none"
-											viewBox="0 0 24 24"
-											stroke="currentColor"
-										>
-											<path
-												stroke-linecap="round"
-												stroke-linejoin="round"
-												stroke-width="2"
-												d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
-											/>
-										</svg>
-									</td>
-									<td
-										class="px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200"
-									>
-										<svg
-											xmlns="http://www.w3.org/2000/svg"
-											class="w-6 h-6 text-red-400"
-											fill="none"
-											viewBox="0 0 24 24"
-											stroke="currentColor"
-										>
-											<path
-												stroke-linecap="round"
-												stroke-linejoin="round"
-												stroke-width="2"
-												d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-											/>
-										</svg>
-									</td>
-								</tr>
-								<tr>
-									<td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-										<div class="flex items-center">
-											<div class="flex-shrink-0 w-10 h-10">
-												<img
-													class="w-10 h-10 rounded-full"
-													src="https://source.unsplash.com/user/erondu"
-													alt="admin dashboard ui"
-												/>
-											</div>
-
-											<div class="ml-4">
-												<div class="text-sm font-medium leading-5 text-gray-900">John Doe</div>
-											</div>
-										</div>
-									</td>
-
-									<td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-										<div class="text-sm leading-5 text-gray-500">john@example.com</div>
-									</td>
-
-									<td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-										<span
-											class="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full"
-											>Active</span
-										>
-									</td>
-
-									<td
-										class="px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200"
-									>
-										<svg
-											xmlns="http://www.w3.org/2000/svg"
-											class="w-6 h-6 text-blue-400"
-											fill="none"
-											viewBox="0 0 24 24"
-											stroke="currentColor"
-										>
-											<path
-												stroke-linecap="round"
-												stroke-linejoin="round"
-												stroke-width="2"
-												d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
-											/>
-										</svg>
-									</td>
-									<td
-										class="px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200"
-									>
-										<svg
-											xmlns="http://www.w3.org/2000/svg"
-											class="w-6 h-6 text-red-400"
-											fill="none"
-											viewBox="0 0 24 24"
-											stroke="currentColor"
-										>
-											<path
-												stroke-linecap="round"
-												stroke-linejoin="round"
-												stroke-width="2"
-												d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-											/>
-										</svg>
-									</td>
-								</tr>
-								<tr>
-									<td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-										<div class="flex items-center">
-											<div class="flex-shrink-0 w-10 h-10">
-												<img
-													class="w-10 h-10 rounded-full"
-													src="https://source.unsplash.com/user/erondu"
-													alt="admin dashboard ui"
-												/>
-											</div>
-
-											<div class="ml-4">
-												<div class="text-sm font-medium leading-5 text-gray-900">John Doe</div>
-											</div>
-										</div>
-									</td>
-
-									<td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-										<div class="text-sm leading-5 text-gray-500">john@example.com</div>
-									</td>
-
-									<td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-										<span
-											class="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full"
-											>Active</span
-										>
-									</td>
-
-									<td
-										class="px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200"
-									>
-										<svg
-											xmlns="http://www.w3.org/2000/svg"
-											class="w-6 h-6 text-blue-400"
-											fill="none"
-											viewBox="0 0 24 24"
-											stroke="currentColor"
-										>
-											<path
-												stroke-linecap="round"
-												stroke-linejoin="round"
-												stroke-width="2"
-												d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
-											/>
-										</svg>
-									</td>
-									<td
-										class="px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200"
-									>
-										<svg
-											xmlns="http://www.w3.org/2000/svg"
-											class="w-6 h-6 text-red-400"
-											fill="none"
-											viewBox="0 0 24 24"
-											stroke="currentColor"
-										>
-											<path
-												stroke-linecap="round"
-												stroke-linejoin="round"
-												stroke-width="2"
-												d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-											/>
-										</svg>
-									</td>
-								</tr>
-								<tr>
-									<td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-										<div class="flex items-center">
-											<div class="flex-shrink-0 w-10 h-10">
-												<img
-													class="w-10 h-10 rounded-full"
-													src="https://source.unsplash.com/user/erondu"
-													alt="admin dashboard ui"
-												/>
-											</div>
-
-											<div class="ml-4">
-												<div class="text-sm font-medium leading-5 text-gray-900">John Doe</div>
-											</div>
-										</div>
-									</td>
-
-									<td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-										<div class="text-sm leading-5 text-gray-500">john@example.com</div>
-									</td>
-
-									<td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-										<span
-											class="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full"
-											>Active</span
-										>
-									</td>
-
-									<td
-										class="px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200"
-									>
-										<svg
-											xmlns="http://www.w3.org/2000/svg"
-											class="w-6 h-6 text-blue-400"
-											fill="none"
-											viewBox="0 0 24 24"
-											stroke="currentColor"
-										>
-											<path
-												stroke-linecap="round"
-												stroke-linejoin="round"
-												stroke-width="2"
-												d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
-											/>
-										</svg>
-									</td>
-									<td
-										class="px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200"
-									>
-										<svg
-											xmlns="http://www.w3.org/2000/svg"
-											class="w-6 h-6 text-red-400"
-											fill="none"
-											viewBox="0 0 24 24"
-											stroke="currentColor"
-										>
-											<path
-												stroke-linecap="round"
-												stroke-linejoin="round"
-												stroke-width="2"
-												d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-											/>
-										</svg>
-									</td>
-								</tr>
-								<tr>
-									<td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-										<div class="flex items-center">
-											<div class="flex-shrink-0 w-10 h-10">
-												<img
-													class="w-10 h-10 rounded-full"
-													src="https://source.unsplash.com/user/erondu"
-													alt="admin dashboard ui"
-												/>
-											</div>
-
-											<div class="ml-4">
-												<div class="text-sm font-medium leading-5 text-gray-900">John Doe</div>
-											</div>
-										</div>
-									</td>
-
-									<td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-										<div class="text-sm leading-5 text-gray-500">john@example.com</div>
-									</td>
-
-									<td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-										<span
-											class="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full"
-											>Active</span
-										>
-									</td>
-
-									<td
-										class="px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200"
-									>
-										<svg
-											xmlns="http://www.w3.org/2000/svg"
-											class="w-6 h-6 text-blue-400"
-											fill="none"
-											viewBox="0 0 24 24"
-											stroke="currentColor"
-										>
-											<path
-												stroke-linecap="round"
-												stroke-linejoin="round"
-												stroke-width="2"
-												d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
-											/>
-										</svg>
-									</td>
-									<td
-										class="px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200"
-									>
-										<svg
-											xmlns="http://www.w3.org/2000/svg"
-											class="w-6 h-6 text-red-400"
-											fill="none"
-											viewBox="0 0 24 24"
-											stroke="currentColor"
-										>
-											<path
-												stroke-linecap="round"
-												stroke-linejoin="round"
-												stroke-width="2"
-												d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-											/>
-										</svg>
-									</td>
-								</tr>
+											<td
+												class="px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200"
+											>
+												<svg
+													xmlns="http://www.w3.org/2000/svg"
+													class="w-6 h-6 text-blue-400"
+													fill="none"
+													viewBox="0 0 24 24"
+													stroke="currentColor"
+												>
+													<path
+														stroke-linecap="round"
+														stroke-linejoin="round"
+														stroke-width="2"
+														d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+													/>
+												</svg>
+											</td>
+											<td
+												class="px-6 py-4 text-sm leading-5 text-gray-500 whitespace-no-wrap border-b border-gray-200"
+											>
+												<svg
+													xmlns="http://www.w3.org/2000/svg"
+													class="w-6 h-6 text-red-400"
+													fill="none"
+													viewBox="0 0 24 24"
+													stroke="currentColor"
+												>
+													<path
+														stroke-linecap="round"
+														stroke-linejoin="round"
+														stroke-width="2"
+														d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+													/>
+												</svg>
+											</td>
+										</tr>
+									{/each}
+								{/await}
 							</tbody>
 						</table>
 					</div>
