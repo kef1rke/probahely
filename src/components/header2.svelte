c8e42ce92925732b6cb1752cffd3a3bab235f6d5
kocsisbarnabas1 <kocsisbarnabas1@gmail.com>
2022-04-27 17:40:58 +0200

    signout

--- src/components/header2.svelte
+++ src/components/header2.svelte
@@ -93,10 +93,12 @@
 						</li>
 						<li>
 							<form action="/logout" method="get">
-								<button
+								<a
+									href="/logout"
+									method="get"
 									class="block float-left w-full py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-									>Sign out
-								</button>
+									>Sign out</a
+								>
 							</form>
 						</li>
 					</ul>
