86745a8973be47fca34133e9b7eda51d224d6073
kocsisbarnabas1 <kocsisbarnabas1@gmail.com>
2022-05-04 12:23:38 +0200

    merge

---
+++ src/components/header.svelte
@@ -0,0 +1,112 @@
+<script>
+	import { onMount } from 'svelte';
+	import { scale } from 'svelte/transition';
+
+	let show = false; // menu state
+	let menu = null; // menu wrapper DOM reference
+
+	import { page } from '$app/stores';
+	export let session;
+
+	onMount(() => {
+		const handleOutsideClick = (event) => {
+			if (show && !menu.contains(event.target)) {
+				show = false;
+			}
+		};
+
+		const handleEscape = (event) => {
+			if (show && event.key === 'Escape') {
+				show = false;
+			}
+		};
+
+		// add events when element is added to the DOM
+		document.addEventListener('click', handleOutsideClick, false);
+		document.addEventListener('keyup', handleEscape, false);
+
+		// remove events when element is removed from the DOM
+		return () => {
+			document.removeEventListener('click', handleOutsideClick, false);
+			document.removeEventListener('keyup', handleEscape, false);
+		};
+	});
+
+	async function logout(e) {
+		const response = await fetch('/logout', {
+			method: 'post',
+			body: new FormData(e.target)
+		});
+		if (response.ok) window.location = '/';
+		else alert(await response.text());
+	}
+</script>
+
+<header class="text-gray-600 body-font m-1">
+	<div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+		<a href="/" class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				stroke="currentColor"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="2"
+				class="w-10 h-10 text-white p-2 bg-gray-500 rounded-full"
+				viewBox="0 0 24 24"
+			>
+				<path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" />
+			</svg>
+			<span class="ml-3 text-xl">Próbaterem</span>
+		</a>
+		<nav
+			class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center"
+		>
+			<a class="mr-5 hover:text-gray-900" class:active={$page.url.path === '/'} href="/">Home</a>
+			<a class="mr-5 hover:text-gray-900" class:active={$page.url.path === '/about'} href="/about"
+				>Rólunk</a
+			>
+			<a
+				class="mr-5 hover:text-gray-900"
+				class:active={$page.url.path === '/idopontfoglalas'}
+				href="/idopontfoglalas">Időpontfoglalás</a
+			>
+		</nav>
+		{#if session != null}
+			<div class="relative z-10" bind:this={menu}>
+				<div>
+					<button
+						on:click={() => (show = !show)}
+						class="menu focus:outline-none focus:shadow-solid"
+					>
+						<img
+							class="w-10 rounded-full"
+							src="https://avatars.githubusercontent.com/u/67946056?v=4"
+							alt=""
+						/></button
+					>
+
+					{#if show}
+						<div
+							class="origin-top-right absolute right-0 w-48 py-2 mt-1 bg-gray-800
+          rounded shadow-md"
+						>
+							<button class="block w-full text-white px-4 py-2 hover:bg-gray-500 "
+								><a href="/profile">Profile</a></button
+							>
+							<form action="/logout" method="get">
+								<button class="block w-full px-4 py-2 text-white hover:bg-gray-500 ">Logout</button>
+							</form>
+						</div>
+					{/if}
+				</div>
+			</div>
+		{:else}
+			<button
+				class="text-white bg-gray-500 border-0 py-2 px-8 focus:outline-none hover:bg-gray-400 rounded text-lg"
+			>
+				<a class:active={$page.url.path === '/login'} href="/login">Bejelentkezés</a>
+			</button>
+		{/if}
+	</div>
+</header>
--- src/routes/admin.svelte
+++ src/routes/admin.svelte
@@ -1,20 +1,7 @@
-<script context="module">
-	import supabase from '$lib/db';
-	export async function load({ url, params, fetch, session, context }) {
-		async function getUserData() {
-			const { data: dat, error: err } = await supabase
-				.from('users')
-				.select('*')
-				.eq('email', session);
-			let userId = dat[0].id;
-
-			const { data, error } = await supabase
-				.from('users')
-				.select('id, email, zenesz_nev, profile_picture_url, Zenekarok(zenekar_nev), user_lvl')
-				.order('zenesz_nev')
-				.eq('id', await userId);
-			return await data[0].user_lvl;
-		}
+<script>
+	import { navOptions } from '../components/nav.svelte'; // import application navigation
+	let selected = navOptions[0]; // keep track of the selected 'page' object (default to the about component since we must have local db connection established first)
+	let intSelected = 0; // selected page index
 
 		if (!session) {
 			return {
@@ -37,4 +24,58 @@
 	}
 </script>
 
-<div>Admin page</div>
+<div class="flex min-h-full">
+	<div class="px-4 py-2 bg-gray-200 bg-indigo-600 lg:w-1/4">
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			class="inline w-8 h-8 text-white lg:hidden"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke="currentColor"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="2"
+				d="M4 6h16M4 12h16M4 18h16"
+			/>
+		</svg>
+		<div class="hidden lg:block">
+			<div class="my-2 mb-6">
+				<h1 class="text-2xl font-bold text-white">Admin Dashboard</h1>
+			</div>
+			<ul>
+				{#each navOptions as option, i}
+					<li
+						class={intSelected == i
+							? 'mb-2 bg-gray-800 rounded shadow'
+							: 'mb-2 rounded hover:shadow hover:bg-gray-800'}
+					>
+						<button
+							class="w-full text-left px-3 py-2 font-bold text-white"
+							on:click={changeComponent}
+							id={i}
+							role="tab">{option.page}</button
+						>
+					</li>
+				{/each}
+				<li class="mb-2 rounded hover:shadow hover:bg-gray-800">
+					<a role="tab" href="/">
+						<button class="w-full text-left px-3 py-2 font-bold text-white"
+							>Go back to website</button
+						></a
+					>
+				</li>
+			</ul>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-sm-12">
+			<div class="p-2">
+				<!-- this is where our main content is placed -->
+				<svelte:component this={selected.component} />
+			</div>
+		</div>
+	</div>
+</div>
